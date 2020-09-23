497cbcc3e1c76c49a219bdfc004b6c566ca911c1
whendev <whendel.morais.9@gmail.com>
2020-09-23 16:02:02 -0300

    :bug: :white_check_mark: exercise solved

--- src/main/java/com/example/project/CupomNF.java
+++ src/main/java/com/example/project/CupomNF.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CupomNF {
-    private List<String> erros = new ArrayList<String>();
     private Loja infoLoja;
     private String texto;
 
